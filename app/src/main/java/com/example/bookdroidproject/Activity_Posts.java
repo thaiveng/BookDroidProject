1d726f6cb4550b6ba6647b1ce18250579d1c87ba
Tay <tay.uorn@gmail.com>
2019-05-15 15:46:23 +0700

    update more

--- app/src/main/java/com/example/bookdroidproject/Activity_Posts.java
+++ app/src/main/java/com/example/bookdroidproject/Activity_Posts.java
@@ -12,18 +12,9 @@ public class Activity_Posts extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity__posts);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_posts);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
 }
